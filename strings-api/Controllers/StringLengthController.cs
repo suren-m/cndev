e80009f14ba8cb4a8b8625da5ee3880a2237fc10
Suren Mohandass <sumohand@microsoft.com>
2021-02-15 13:28:43 +0000

    initial setup

---
+++ strings-api/Controllers/StringLengthController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceStack.Redis;
+
+namespace strings_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringLengthController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly RedisManagerPool _redisPool;
+
+        public StringLengthController(ILogger<WeatherForecastController> logger, RedisManagerPool redisPool)
+        {
+            _logger = logger;
+            _redisPool = redisPool;
+        }
+
+        [HttpGet]
+        [Route("{input}/")]
+        public StringResult Get(string input)
+        {            
+            var res = new StringResult();
+            res.Input = input;
+            using (var cache = _redisPool.GetClient())
+            {              
+                var data = cache.Get<string>(input);
+                if(data !=null) {
+                    res.CacheHit = true;                    
+                    int.TryParse(data, out int len);
+                    res.Length = len;
+                } else {
+                    var length = input.Length;
+                    res.Length = length;
+                    res.CacheHit = false;                                    
+                    cache.Set(input, length);                    
+                }                
+            }                 
+            return res;          
+        }
+
+        public class StringResult {
+            public string Input { get; set; }
+            public int Length {get; set; }
+
+            public bool CacheHit { get; set;}
+        }
+    }
+}
---
+++ strings-frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace strings_frontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly IHttpClientFactory _clientFactory;
+        
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string StringInput { get; set;}
+
+        public StringResult StringResult { get; set;}
+        
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+            StringResult = new StringResult();
+        }
+
+        // public void OnGet()
+        // {
+        //     var test = StringResult;
+        // }
+
+        public async Task OnGetAsync(string input){        
+
+            if (string.IsNullOrEmpty(input)) {
+                return;
+            }             
+
+            var client = _clientFactory.CreateClient();
+            _logger.LogInformation("\n...Retrieving Data...\n");
+
+            var request = new System.Net.Http.HttpRequestMessage();
+            var query = $"http://strings-api.strings-app/stringlength/{input}";
+            //var query = $"http://localhost:8080/stringlength/{input}";
+            request.RequestUri = new Uri(query);
+            
+            var response = await client.SendAsync(request);
+            var jsonResult = await response.Content.ReadAsStringAsync();
+
+            StringResult = JsonSerializer.Deserialize<StringResult>(
+                    jsonResult,
+                    new JsonSerializerOptions { 
+                        PropertyNameCaseInsensitive = true 
+                    }
+            );
+
+            _logger.LogInformation("\n...Finished Retrieving Data...\n");            
+        }  
+    }
+
+    public class StringResult {
+        
+        public string Input { get; set; }
+        public int Length { get; set; }
+
+        public bool CacheHit { get; set; }
+    }
+}
